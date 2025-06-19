c34a35e13845450c352f02aa55b642af4c844650
developer-rudra <panigrahirudraprasad1@gmail.com>
2025-06-19 19:45:33 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline();
+function loadingAnimation() {
+    var tl = gsap.timeline();
 
 tl.from(".line h1", {
     y: 150,
@@ -28,7 +29,7 @@ tl.to(".line h2",{
 tl.to("#loader", {
     opacity: 0,
     duration: 0.2,
-    delay: 3
+    delay: 0
 });
 tl.from("#page1",{
     delay:0.2,
@@ -39,3 +40,24 @@ tl.from("#page1",{
 tl.to("#loader",{
     display:"none"
 })
+tl.from("#nav", {
+    opacity:0
+})
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
+    y:120,
+    stagger:0.2
+})
+}
+loadingAnimation()
+
+
+function cursorAnimation(){
+    document.addEventListener("mousemove", function(dets){
+    gsap.to("#crsr", {
+        left:dets.x,
+        top:dets.y
+    })
+})
+Shery.makeMagnet("#nav-part2 h4");
+}
+cursorAnimation()
