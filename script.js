f23ac628479cfe09aacd885cb86ddacdfa7034d6
developer-rudra <panigrahirudraprasad1@gmail.com>
2025-06-20 23:36:11 +0530

    locomotive is applied

--- script.js
+++ script.js
@@ -1,63 +1,99 @@
+function locomotiveAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Initialize Locomotive Scroll
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true
+    });
+
+    // Sync ScrollTrigger with Locomotive Scroll
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    // Proxy methods for ScrollTrigger to work with Locomotive
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    ScrollTrigger.refresh();
+}
+
 function loadingAnimation() {
     var tl = gsap.timeline();
 
-tl.from(".line h1", {
-    y: 150,
-    stagger: 0.25,
-    duration: 0.5,
-    delay: 0.5
-});
-tl.from("#line1-part1",{
-    opacity: 0,
-    onStart: function () {
-        var h5timer = document.querySelector("#line1-part1 h5");
-        var grow = 0;
+    tl.from(".line h1", {
+        y: 150,
+        stagger: 0.25,
+        duration: 0.5,
+        delay: 0.5
+    });
 
-        setInterval(function () {
-            if (grow < 100) {
-                h5timer.innerHTML = grow++;
-            } else {
-                h5timer.innerHTML = grow;
-            }
-        }, 34);
-    }
-});
-tl.to(".line h2",{
-    animationName:"animee",
-    opacity:0
-})
-tl.to("#loader", {
-    opacity: 0,
-    duration: 0.8,
-    delay: 3.4
-});
-tl.from("#page1",{
-    delay:0.2,
-    y:1600,
-    duration:0.6,
-    ease:Power4
-})
-tl.to("#loader",{
-    display:"none"
-})
-tl.from("#nav", {
-    opacity:0
-})
-tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
-    y:140,
-    stagger:0.2
-})
+    tl.from("#line1-part1", {
+        opacity: 0,
+        onStart: function () {
+            var h5timer = document.querySelector("#line1-part1 h5");
+            var grow = 0;
+
+            setInterval(function () {
+                if (grow < 100) {
+                    h5timer.innerHTML = grow++;
+                } else {
+                    h5timer.innerHTML = grow;
+                }
+            }, 34);
+        }
+    });
+
+    tl.to(".line h2", {
+        animationName: "animee",
+        opacity: 0
+    });
+
+    tl.to("#loader", {
+        opacity: 0,
+        duration: 0.2
+    });
+
+    tl.from("#page1", {
+        delay: 0.2,
+        y: 1600,
+        duration: 0.6,
+        ease: Power4
+    });
+
+    tl.to("#loader", {
+        display: "none"
+    });
+
+    tl.from("#nav", {
+        opacity: 0
+    });
+
+    tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1", {
+        y: 140,
+        stagger: 0.2
+    });
 }
-loadingAnimation()
 
+function cursorAnimation() {
+    document.addEventListener("mousemove", function (dets) {
+        gsap.to("#crsr", {
+            left: dets.x,
+            top: dets.y
+        });
+    });
 
-function cursorAnimation(){
-    document.addEventListener("mousemove", function(dets){
-    gsap.to("#crsr", {
-        left:dets.x,
-        top:dets.y
-    })
-})
-Shery.makeMagnet("#nav-part2 h4");
+    Shery.makeMagnet("#nav-part2 h4");
 }
-cursorAnimation()
+
+// Run animations
+loadingAnimation();
+cursorAnimation();
+locomotiveAnimation();
